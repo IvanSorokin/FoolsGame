32b1aac4d2280df31b3c81ed15967fce0c5cacbc
IvanSorokin <iwan954@mail.ru>
2014-02-26 20:17:58 +0600

    Some of main things added

--- FoolsGame/Program.cs
+++ FoolsGame/Program.cs
@@ -6,11 +6,56 @@ using System.Threading.Tasks;
 
 namespace FoolsGame
 {
+    public enum Suit { Spades, Hearts, Diamonds, Clubs };
+    public enum Nominal { Ace, King, Queen, Jack, Ten, Nine, Eight, Seven, Six };
+
+    interface IPlayer
+    {
+        void FirstMove();
+        void AdditionalMove();
+        void Defend();
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Let's begin!");
+            var pack = new List<Card>(); //try to make a pack
+
+            foreach (Suit suit in (Suit[])Enum.GetValues(typeof(Suit)))
+                foreach (Nominal nominal in (Nominal[])Enum.GetValues(typeof(Nominal)))
+                {
+                    Card card = new Card(suit, nominal);
+                    pack.Add(card);
+                }
         }
+    }
+
+    public class Card
+    {
+        public Suit suit
+        {
+            get;
+            set;
+        }
+
+        public Nominal nominal
+        {
+            get;
+            set;
+        }
+
+        public Card (Suit _suit, Nominal _nominal)
+        {
+            suit = _suit;
+            nominal = _nominal;
+        }
+    }
+
+    class Arbiter
+    {
+        void IsPossibleTurn(); //etc
+        void FormInitialPack();
     }
 }
