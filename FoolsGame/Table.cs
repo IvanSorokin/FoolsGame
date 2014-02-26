0f2c3ce5805b20a99a46f18a8487635adc4167ff
IvanSorokin <iwan954@mail.ru>
2014-02-26 23:02:17 +0600

    Classes

---
+++ FoolsGame/Table.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoolsGame
+{
+    class Table
+    {
+        public List<Tuple<Card, Card>> tablePosition
+        {
+            get;
+            set;
+        }
+    }
+}
