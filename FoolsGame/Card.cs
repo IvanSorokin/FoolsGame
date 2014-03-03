95542d8307a1866d99204df835ffbda9dd72e529
FoKycHuK <FoKycHuK@users.noreply.github.com>
2014-03-03 18:47:12 +0600

    Update Card.cs
    hard rollback

--- FoolsGame/Card.cs
+++ FoolsGame/Card.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace FoolsGame
 {
-    public enum Suit { spades, hearts, diamonds, clubs };
-    public enum Nominal { vA = 8, vK = 7, vQ = 6, vJ = 5, vT = 4, v9 = 3, v8 = 2, v7 = 1, v6 = 0};
+    public enum Suit { Spades, Hearts, Diamonds, Clubs };
+    public enum Nominal { Ace = 8, King = 7, Queen = 6, Jack = 5, Ten = 4, Nine = 3, Eight = 2, Seven = 1, Six = 0};
 	//digits for more comfortable comp-s!
 
     public class Card
