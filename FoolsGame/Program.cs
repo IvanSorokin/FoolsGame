0acfab760c70d89b796f7f4b6819d2dcc0b301f7
Mironov-Alexey <mironov.alexey.fiit.urfu@gmail.com>
2014-03-05 21:52:48 +0600

    small changes

--- FoolsGame/Program.cs
+++ FoolsGame/Program.cs
@@ -16,30 +16,30 @@ namespace FoolsGame
         static public Stack<Card> pack; 
         static void Main(string[] args)
         {
-            //Console.WriteLine("Let's play!"); 
-            //pack = Arbiter.FormInitialPack();
-            //trumpCard = pack.First();
-            //players = new Player[countOfPlayer];
-            //for (int i = 0; i < countOfPlayer; i++)
-            //{
-            //    players[i] = new Player();
-            //    Arbiter.GiveCardsToPlayer(players[i], pack);
-            //}
-            //foreach (Nominal nominal in (Nominal[])Enum.GetValues(typeof(Nominal)))
-            //{
-            //    Card card = new Card(trumpCard.suit, nominal);
-            //    for (int i = 0; i < countOfPlayer; i++)
-            //        if (players[i].hand.Contains(card))
-            //        {
-            //            turn = (i + 1) % countOfPlayer;
-            //            break;
-            //        }
-            //}
-            //while (true)
-            //{
-            //    //тут типа ход игры)))
-                
-            //}
+            Console.WriteLine("Let's play!");
+            pack = Arbiter.FormInitialPack();
+            trumpCard = pack.First();
+            players = new Player[countOfPlayer];
+            for (int i = 0; i < countOfPlayer; i++)
+            {
+                players[i] = new Player();
+                Arbiter.GiveCardsToPlayer(players[i], pack);
+            }
+            foreach (Nominal nominal in (Nominal[])Enum.GetValues(typeof(Nominal)))
+            {
+                Card card = new Card(trumpCard.suit, nominal);
+                for (int i = 0; i < countOfPlayer; i++)
+                    if (players[i].hand.Contains(card))
+                    {
+                        turn = (i + 1) % countOfPlayer;
+                        break;
+                    }
+            }
+            while (true)
+            {
+                //тут типа ход игры)))
+
+            }
             //rollback
         }
     }
