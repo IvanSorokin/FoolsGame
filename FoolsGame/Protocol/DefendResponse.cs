a109b7fd84ea68d2fa72a2ecaa8595da95734cb2
Mironov-Alexey <mironov.alexey.fiit.urfu@gmail.com>
2014-03-05 21:34:11 +0600

    Добавлены классы MoveInfo, DefendResponse, AttackResponse.cs

---
+++ FoolsGame/Protocol/DefendResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoolsGame
+{
+    class DefendInfo
+    {
+        public List<Card> BeatenCards;
+        public List<Card> PlayerHand;
+        public Table CurrentTable;
+    }
+}
---
+++ FoolsGame/Protocol/MoveInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoolsGame
+{
+    public class MoveInfo
+    {
+        public List<Card> PlayerHand;
+        public Table CurrentTable;
+        public Suit Suit = Program.trumpCard.suit;
+        public int CountOfDefenseCards
+        {
+            get;
+            set;
+        }
+    }
+}
