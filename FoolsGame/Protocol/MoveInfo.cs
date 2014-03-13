585355fa6657b23362e590e39d4906cd789cc6d6
okulovsky <yuri.okulovsky@gmail.com>
2014-03-13 17:29:05 +0600

    Update MoveInfo.cs
    ответы на вопросы

--- FoolsGame/Protocol/MoveInfo.cs
+++ FoolsGame/Protocol/MoveInfo.cs
@@ -16,5 +16,20 @@ namespace FoolsGame
             get;
             set;
         }
+        
+        /*
+        public Card[] PlayerHand;
+        public List<PairCard> CurrentTable; //здесь только побитые карты, это решит проблемы null/не null
+        public Card[] NotBeatenCards; //передавать отдельно, так удобнее
+        public Suit Suit = Program.trumpCard.suit;
+        public int[] CardsCounts;
+        public int PlayerNumber; //какой у тебя номер в предыдущем массиве CardsCounts
+        public int CountOfDefenseCards
+        {
+            get;
+            set;
+        }
+        
+        */
     }
 }
